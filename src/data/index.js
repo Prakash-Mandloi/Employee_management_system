c5102c9eda42086ccf59bc0f52fddba101f6a4b6
Prakash-Mandloi <b20222@students.iitmandi.ac.in>
2023-11-08 18:47:00 +0530

    employee management system

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Santosh',
+    lastName: 'Mohan',
+    email: 'santosh@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Ankit',
+    lastName: 'Deewan',
+    email: 'ankit@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Rohan',
+    lastName: 'bhattnagar',
+    email: 'rohan@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'Kajal',
+    lastName: 'Ronak',
+    email: 'kajal@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Andrew',
+    lastName: 'Grant',
+    email: 'andrewo@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'Laura',
+    lastName: 'Lurie',
+    email: 'laura@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'Gunjan',
+    lastName: 'bishnoi',
+    email: 'gunjan@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'Jesica',
+    lastName: 'rathi',
+    email: 'jesica@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'Mithun',
+    lastName: 'saxena',
+    email: 'mithun@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'lawrence',
+    lastName: 'bishnoi',
+    email: 'bishnoi@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
